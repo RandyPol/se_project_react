71574f29f2a0958fd74dbf1c9a934c215c233fac
Ran Pol <rnd809@gmail.com>
2023-01-31 23:28:35 -0500

    Create the initial arrow components for each commonent.

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,9 +1,21 @@
-import './App.css';
+import './App.css'
+
+import Header from '../Header/Header'
+import Main from '../Main/Main'
+import Footer from '../Footer/Footer'
+import ModalWithForm from '../ModalWithForm/ModalWithForm'
+import ItemModal from '../ItemModal/ItemModal'
 
 function App() {
   return (
-    <></>
-   )
+    <>
+      <Header />
+      <Main />
+      <Footer />
+      <ModalWithForm />
+      <ItemModal />
+    </>
+  )
 }
 
-export default App;
+export default App
---
+++ src/components/ItemModal/ItemModal.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import './ItemModal.css'
+
+const ItemModal = () => {
+  return (
+    <div>ItemModal</div>
+  )
+}
+
+export default ItemModal
