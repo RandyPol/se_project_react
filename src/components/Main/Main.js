ee8ad9f5c6740328d9f1e7ad2068e2195e43ae19
Ran Pol <rnd809@gmail.com>
2023-02-03 03:14:24 -0500

    Create the foundation for the main content of the app.

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,9 +1,27 @@
 import React from 'react'
 import './Main.css'
+import WeatherCard from './WeatherCard/WeatherCard'
+import ListItemCard from './ItemCard/ListItemCard'
 
-const Main = () => {
+const Main = ({ weatherData, clothingItems }) => {
+  console.log(weatherData.temp)
+
+  const filterClothes = clothingItems.filter(
+    (item) => item.weather === weatherData.tempDescription
+  )
+
   return (
-    <div>Main</div>
+    <main className="main page__main">
+      <section>
+        <WeatherCard temp={weatherData.temp + '\u00B0'} />
+        <p className="main__todayis-paragraph">Today is {weatherData.temp + '\u00B0'}F / You may want to wear:</p>
+        <ul className="main__card-list">
+          {filterClothes.map((item) => (
+            <ListItemCard key={item._id} item={item} />
+          ))}
+        </ul>
+      </section>
+    </main>
   )
 }
 
