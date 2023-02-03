079e18328dafac42c693d124988b746e91e9b52a
Ran Pol <rnd809@gmail.com>
2023-02-03 03:12:26 -0500

    Create the header foundation: Logo Location Addbutton and username.

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,18 +3,20 @@ import './Header.css'
 import logo from '../../images/Logo.svg'
 import avatar from '../../images/avatar.avif'
 
-const Header = (props) => {
+const Header = ({ name, handleFormToggleOpen }) => {
   const currentDate = new Date().toLocaleString('default', {
     month: 'long',
     day: 'numeric',
   })
 
   return (
-    <header className="header">
+    <header className="header page__headerchild">
       <img className="header__logo" src={logo} alt="WTWR Logo" />
-      <p className="header__location">{`${currentDate}, ${props.name}`}</p>
+      <p className="header__location">{`${currentDate}, ${name}`}</p>
 
-      <button className="header__button-add">+ Add clothes</button>
+      <button className="header__button-add" onClick={handleFormToggleOpen}>
+        + Add clothes
+      </button>
       <div className="header__user-container">
         <p className="header__username">Michael Jordan</p>
         <img
