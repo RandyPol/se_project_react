c6b062289ff929362fa948fe6c34ddd26fb37bde
Ran Pol <rnd809@gmail.com>
2023-02-03 03:10:33 -0500

    Create the foundation for the Modal With Form.

--- src/components/ModalWithForm/ModalWithForm.js
+++ src/components/ModalWithForm/ModalWithForm.js
@@ -1,9 +1,44 @@
 import React from 'react'
 import './ModalWithForm.css'
 
-const ModalWithForm = () => {
+const ModalWithForm = ({
+  title,
+  name,
+  buttonText,
+  handleFormToggleOpen,
+  children,
+  inputValues,
+  setInputValues,
+}) => {
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    // Perform submit action with inputValues
+    setInputValues({ name: '', image: '', weather: '' })
+    console.log(inputValues)
+    handleFormToggleOpen()
+  }
+
   return (
-    <div>ModalWithForm</div>
+    <div className={`modal modal_type_${name}`}>
+      <form name={name} onSubmit={handleSubmit}>
+        <div className="modal__header">
+          <h2 className="modal__title">{title}</h2>
+          <button
+            type="button"
+            className="modal__close-button"
+            onClick={handleFormToggleOpen}
+          >
+            ×
+          </button>
+        </div>
+        {children}
+        <fieldset className="modal__form-group">
+          <button type="submit" className="modal__submit-button">
+            {buttonText}
+          </button>
+        </fieldset>
+      </form>
+    </div>
   )
 }
 
