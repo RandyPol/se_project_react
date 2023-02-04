d71e503a5f5e68bea1c5247212d0434f13750359
Ran Pol <rnd809@gmail.com>
2023-02-03 22:37:10 -0500

    Create the basic strudcture and stylesheet for the ItemModal.

--- src/components/ItemModal/ItemModal.js
+++ src/components/ItemModal/ItemModal.js
@@ -1,9 +1,29 @@
 import React from 'react'
 import './ItemModal.css'
+import closeIcon from '../../images/CloseModalIcon.svg'
 
-const ItemModal = () => {
+const ItemModal = ({ name, cardItem, handleItemModalToggleOpen }) => {
   return (
-    <div>ItemModal</div>
+    <div className={`modal modal_type_${name}`}>
+      <div className="modal__container">
+        <button
+          type="button"
+          className="modal__close-button"
+          onClick={handleItemModalToggleOpen}
+        >
+          <img className="modal__close-icon" src={closeIcon} alt="Close icon" />
+        </button>
+        <img
+          className="modal__card-image"
+          src={cardItem.link}
+          alt={cardItem.name}
+        ></img>
+        <div className="modal__title-weather-container">
+          <p className="modal__card-paragraphs">{cardItem.name}</p>
+          <p className="modal__card-paragraphs">Weather: {cardItem.weather}</p>
+        </div>
+      </div>
+    </div>
   )
 }
 
