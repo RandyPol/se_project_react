e05716f234a8e3076ca57cd66158734530ff37ac
Ran Pol <rnd809@gmail.com>
2023-02-05 01:42:01 -0500

    Feature: create the funcionality of the form validation.

--- src/components/ModalWithForm/ModalWithForm.js
+++ src/components/ModalWithForm/ModalWithForm.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import './ModalWithForm.css'
 import closeIcon from '../../images/CloseModalIcon.svg'
+import LabelWithError from './LabelWithError/LabelWithError'
 
 const ModalWithForm = ({
   title,
@@ -8,8 +9,48 @@ const ModalWithForm = ({
   buttonText,
   handleFormToggleOpen,
   children,
-  inputValues,
 }) => {
+  const [inputValues, setInputValues] = React.useState({
+    name: '',
+    image: '',
+    weather: '',
+  })
+  const [validity, setValidity] = React.useState({})
+
+  // Check the inputValues and the validity to see if the submit button should be disabled
+  const isSubmitDisabled = () => {
+    const isInputValueEmpty = Object.values(inputValues).some(
+      (inputValue) => inputValue === ''
+    )
+
+    const validityEmpty = Object.values(validity).some((validity) => {
+      return validity.valid === false
+    })
+
+    return isInputValueEmpty || validityEmpty
+  }
+
+  // This is the function to handle the input changes for the modal form
+  const handleInputChange = (e) => {
+    let input = e.target
+    let id = input.id
+    setInputValues({
+      ...inputValues,
+      [input.name]: input.value,
+    })
+    // console.log(input.validity.valid)
+    if (input.validity.valid) {
+      // console.log('Updated the validity is not valid')
+      setValidity({ ...validity, [id]: { valid: true, message: '' } })
+    } else {
+      // console.log('Updated the validity')
+      setValidity({
+        ...validity,
+        [id]: { valid: false, message: input.validationMessage },
+      })
+    }
+  }
+
   // Handle the escape key to close the modal
   React.useEffect(() => {
     const handleKeyPress = (event) => {
@@ -31,18 +72,54 @@ const ModalWithForm = ({
         handleFormToggleOpen()
       }
     }
-    document.addEventListener('click', handleClickOutsideForm)
+    document.addEventListener('mousedown', handleClickOutsideForm)
 
     return () => {
-      document.removeEventListener('click', handleClickOutsideForm)
+      document.removeEventListener('mousedown', handleClickOutsideForm)
     }
   }, [handleFormToggleOpen])
+
+  // Function to handle the children type passed by the App component
+  const handleChild = (child) => {
+    if (child.type === 'input') {
+      let id = child.props.id
+      if (child.props.type === 'radio') {
+        return React.cloneElement(child, {
+          onChange: handleInputChange,
+          checked: inputValues.weather === child.props.value,
+        })
+      }
+      return React.cloneElement(child, {
+        onChange: handleInputChange,
+        value: inputValues[id],
+        className:
+          validity[id] && !validity[id].valid
+            ? 'form__input form__input-error'
+            : 'form__input',
+      })
+    } else if (child.type === 'label') {
+      return (
+        <LabelWithError
+          htmlFor={child.props.htmlFor}
+          validity={validity}
+          textContent={child.props.children}
+          labelClassName={child.props.className}
+        />
+      )
+    } else if (child.props && child.props.children) {
+      let children = React.Children.map(child.props.children, handleChild)
+      return React.cloneElement(child, { children })
+    } else {
+      return child
+    }
+  }
 
   const handleSubmit = (e) => {
     e.preventDefault()
     // Perform submit action with inputValues
     console.log(inputValues)
-    handleFormToggleOpen()
+    console.log(validity)
+    // handleFormToggleOpen()
   }
 
   return (
@@ -51,6 +128,7 @@ const ModalWithForm = ({
         className="form modal__container"
         name={name}
         onSubmit={handleSubmit}
+        noValidate
       >
         <h2 className="form__title">{title}</h2>
         <button
@@ -61,9 +139,21 @@ const ModalWithForm = ({
           <img className="form__close-icon" src={closeIcon} alt="Close icon" />
         </button>
 
-        {children}
+        <div className="form__body">
+          {/* Children represent the list of inputs given by the App/parent component */}
+          {React.Children.map(children, handleChild)}
+        </div>
+
         <fieldset className="form__fieldset">
-          <button type="submit" className="form__submit-button">
+          <button
+            type="submit"
+            disabled={isSubmitDisabled()}
+            className={
+              isSubmitDisabled()
+                ? 'form__submit-button_disabled'
+                : 'form__submit-button_base'
+            }
+          >
             {buttonText}
           </button>
         </fieldset>
