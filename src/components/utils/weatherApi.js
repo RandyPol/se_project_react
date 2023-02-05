4a8082354fc8646bc0a035506d5e0aab81cc98a7
Ran Pol <rnd809@gmail.com>
2023-02-05 01:41:09 -0500

    Comment out the weatherApi constant to prevent React flaging it as an error.

--- src/components/utils/weatherApi.js
+++ src/components/utils/weatherApi.js
@@ -2,49 +2,49 @@ import { coordinates, AUTH_TOKEN } from './constants'
 
 const API_URL = `https://api.openweathermap.org/data/2.5/weather?lat=${coordinates.lat}&lon=${coordinates.long}&units=imperial&appid=${AUTH_TOKEN}`
 
-const weatherObject = {
-  coord: {
-    lon: -77.0369,
-    lat: 38.9072,
-  },
-  weather: [
-    {
-      id: 800,
-      main: 'Clear',
-      description: 'clear sky',
-      icon: '01n',
-    },
-  ],
-  base: 'stations',
-  main: {
-    temp: 29.26,
-    feels_like: 29.26,
-    temp_min: 26.29,
-    temp_max: 32.32,
-    pressure: 1026,
-    humidity: 67,
-  },
-  visibility: 10000,
-  wind: {
-    speed: 0,
-    deg: 0,
-  },
-  clouds: {
-    all: 0,
-  },
-  dt: 1675303823,
-  sys: {
-    type: 2,
-    id: 2008521,
-    country: 'US',
-    sunrise: 1675253679,
-    sunset: 1675290519,
-  },
-  timezone: -18000,
-  id: 4140963,
-  name: 'Washington D.C.',
-  cod: 200,
-}
+// const weatherObject = {
+//   coord: {
+//     lon: -77.0369,
+//     lat: 38.9072,
+//   },
+//   weather: [
+//     {
+//       id: 800,
+//       main: 'Clear',
+//       description: 'clear sky',
+//       icon: '01n',
+//     },
+//   ],
+//   base: 'stations',
+//   main: {
+//     temp: 29.26,
+//     feels_like: 29.26,
+//     temp_min: 26.29,
+//     temp_max: 32.32,
+//     pressure: 1026,
+//     humidity: 67,
+//   },
+//   visibility: 10000,
+//   wind: {
+//     speed: 0,
+//     deg: 0,
+//   },
+//   clouds: {
+//     all: 0,
+//   },
+//   dt: 1675303823,
+//   sys: {
+//     type: 2,
+//     id: 2008521,
+//     country: 'US',
+//     sunrise: 1675253679,
+//     sunset: 1675290519,
+//   },
+//   timezone: -18000,
+//   id: 4140963,
+//   name: 'Washington D.C.',
+//   cod: 200,
+// }
 
 // here is the code of the checking the response status
 function checkResponse(res) {
@@ -58,9 +58,9 @@ export function request() {
   return fetch(API_URL).then(checkResponse)
 }
 
-function celsiusToFahrenheit(celsius) {
-  return (celsius * 9) / 5 + 32
-}
+// function celsiusToFahrenheit(celsius) {
+//   return (celsius * 9) / 5 + 32
+// }
 
 export const weatherDataProcesing = (weatherData) => {
   const {
