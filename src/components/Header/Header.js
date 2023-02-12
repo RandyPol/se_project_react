4840d163fc2a7a5c631309a000c79aae26e97c49
Ran Pol <rnd809@gmail.com>
2023-02-12 01:45:18 -0500

    Update the header component to use the CurrentTemperatureUnitContext.

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,8 +2,13 @@ import React from 'react'
 import './Header.css'
 import logo from '../../images/Logo.svg'
 import avatar from '../../images/avatar.avif'
+import ToggleSwitch from './ToggleSwitch/ToggleSwitch'
+// Import the context value
+import CurrentTemperatureUnitContext from '../../contexts/CurrentTemperatureUnitContext'
 
-const Header = ({ name, handleFormToggleOpen }) => {
+const Header = ({ handleFormToggleOpen }) => {
+  const { weatherData } = React.useContext(CurrentTemperatureUnitContext)
+
   const currentDate = new Date().toLocaleString('default', {
     month: 'long',
     day: 'numeric',
@@ -12,7 +17,9 @@ const Header = ({ name, handleFormToggleOpen }) => {
   return (
     <header className="header page__headerchild">
       <img className="header__logo" src={logo} alt="WTWR Logo" />
-      <p className="header__location">{`${currentDate}, ${name}`}</p>
+      <p className="header__location">{`${currentDate}, ${weatherData.name}`}</p>
+
+      <ToggleSwitch />
 
       <button className="header__button-add" onClick={handleFormToggleOpen}>
         + Add clothes
