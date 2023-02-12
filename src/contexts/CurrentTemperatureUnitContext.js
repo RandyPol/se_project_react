2dbc5568c721a56b70dbcd525dae7afef878d342
Ran Pol <rnd809@gmail.com>
2023-02-12 01:47:24 -0500

    Create the context call  CurrentTemperatureUnitContext.

---
+++ src/contexts/CurrentTemperatureUnitContext.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+/** Create a context for the value of the weather in the prefer temperature unit. */
+
+const CurrentTemperatureUnitContext = React.createContext();
+
+export default CurrentTemperatureUnitContext;
