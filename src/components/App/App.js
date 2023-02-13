ad4cc2febdaa059602d50d3b3d308e907af39faa
Ran Pol <rnd809@gmail.com>
2023-02-13 15:54:15 -0500

    Update the App component to delete the ModalWithForm and Import its replacememnt. Ipmort the api calls.

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -7,24 +7,49 @@ import './App.css'
 import Header from '../Header/Header'
 import Main from '../Main/Main'
 import Footer from '../Footer/Footer'
-import ModalWithForm from '../ModalWithForm/ModalWithForm'
 import ItemModal from '../ItemModal/ItemModal'
 import Profile from '../Profile/Profile'
+import AddItemModal from '../AddItemModal/AddItemModal'
 // Context Data
 import CurrentTemperatureUnitContext from '../../contexts/CurrentTemperatureUnitContext'
-
 // Utils and constants
-import { defaultClothingItems } from '../../utils/constants'
-import { request, weatherDataProcesing } from '../../utils/weatherApi'
+import api from '../../utils/api'
+import {
+  requestWeatherApiData,
+  weatherDataProcesing,
+} from '../../utils/weatherApi'
 
 function App() {
   const [weatherData, setWeatherData] = React.useState({})
   const [isModalFormOpen, setIsModalFormOpen] = React.useState(false)
   const [isItemModalOpen, setIsItemModalOpen] = React.useState(false)
   const [isTempFahrenheit, setIsTempFahrenheit] = React.useState(true)
-
-  // Card item info for the ItemModal
+  // Clothing items from the api
+  const [clothingItems, setClothingItems] = React.useState([])
+  // Card item info for the ItemModal/The Card that was clicked
   const [cardItem, setCardItem] = React.useState({})
+
+  // Api call to get the weather data from the weather api (on mount only once)
+  React.useEffect(() => {
+    requestWeatherApiData()
+      .then((data) => {
+        // Update the weather data in the state after processing the data
+        const processData = weatherDataProcesing(data)
+        console.log(processData)
+        setWeatherData(processData)
+      })
+      .catch((err) => console.log(err))
+  }, [])
+
+  // useEffect to get clothing items from the api and set them to the state
+  React.useEffect(() => {
+    api
+      .getInitialClothingItems()
+      .then((data) => {
+        setClothingItems(data)
+      })
+      .catch((err) => console.log(err))
+  }, [])
 
   // Toggle the temperature unit between Fahrenheit and Celsius
   const handleTempUnitToggle = () => {
@@ -41,15 +66,15 @@ function App() {
     setIsItemModalOpen((prevs) => !prevs)
   }
 
-  React.useEffect(() => {
-    request()
-      .then((data) => {
-        // Update the weather data in the state after processing the data
-        const processData = weatherDataProcesing(data)
-        setWeatherData(processData)
+  // Add a new clothing item to the server
+  const handleAddItemSubmit = (newItem) => {
+    api
+      .postClothingItem(newItem)
+      .then((addedData) => {
+        setClothingItems([addedData, ...clothingItems])
       })
       .catch((err) => console.log(err))
-  }, [])
+  }
 
   return (
     <div className="page">
@@ -57,7 +82,7 @@ function App() {
         <CurrentTemperatureUnitContext.Provider
           value={{
             weatherData,
-            defaultClothingItems,
+            clothingItems,
             isTempFahrenheit,
             handleTempUnitToggle,
             handleFormToggleOpen,
@@ -73,96 +98,22 @@ function App() {
               <Profile />
             </Route>
           </Switch>
-        </CurrentTemperatureUnitContext.Provider>
 
-        <Footer />
-        {isModalFormOpen && (
-          <ModalWithForm
-            title="New garment"
-            name="addGarment"
-            buttonText="Add garment"
-            handleFormToggleOpen={handleFormToggleOpen}
-          >
-            <fieldset className="form__fieldset">
-              <label className="form__label" htmlFor="name">
-                Name
-              </label>
-              <input
-                className="form__input"
-                type="text"
-                id="name"
-                name="name"
-                placeholder="Name"
-                minLength={3}
-                required
-              />
-            </fieldset>
-            <fieldset className="form__fieldset">
-              <label className="form__label" htmlFor="image">
-                Image URL
-              </label>
-              <input
-                className="form__input"
-                type="url"
-                id="image"
-                name="image"
-                placeholder="Image URL"
-                required
-              />
-            </fieldset>
-            <fieldset className="form__fieldset">
-              <p className="form__radio-group-title">
-                Select the weather type:
-              </p>
-              <div className="form__radio-group">
-                <input
-                  className="form__input-radio"
-                  type="radio"
-                  id="hot"
-                  name="weather"
-                  value="Hot"
-                  required
-                />
-                <label className="form__label-radio" htmlFor="hot">
-                  Hot
-                </label>
-              </div>
-              <div className="form__radio-group">
-                <input
-                  className="form__input-radio"
-                  type="radio"
-                  id="warm"
-                  name="weather"
-                  value="Warm"
-                  required
-                />
-                <label className="form__label-radio" htmlFor="warm">
-                  Warm
-                </label>
-              </div>
-              <div className="form__radio-group">
-                <input
-                  className="form__input-radio"
-                  type="radio"
-                  id="cold"
-                  name="weather"
-                  value="Cold"
-                  required
-                />
-                <label className="form__label-radio" htmlFor="cold">
-                  Cold
-                </label>
-              </div>
-            </fieldset>
-          </ModalWithForm>
-        )}
-        {isItemModalOpen && (
-          <ItemModal
-            handleItemModalToggleOpen={handleItemModalToggleOpen}
-            name={'image'}
-            cardItem={cardItem}
-          />
-        )}
+          <Footer />
+          {isModalFormOpen && (
+            <AddItemModal
+              onAddItem={handleAddItemSubmit}
+              handleFormToggleOpen={handleFormToggleOpen}
+            />
+          )}
+          {isItemModalOpen && (
+            <ItemModal
+              handleItemModalToggleOpen={handleItemModalToggleOpen}
+              name={'image'}
+              cardItem={cardItem}
+            />
+          )}
+        </CurrentTemperatureUnitContext.Provider>
       </div>
     </div>
   )
