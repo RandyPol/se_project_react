fc6f143ecc1191f7b8d206a8ad9c9ac70558acfe
Ran Pol <rnd809@gmail.com>
2023-02-13 16:08:24 -0500

    Delete the commets of the weather data.

--- src/utils/weatherApi.js
+++ src/utils/weatherApi.js
@@ -2,50 +2,6 @@ import { coordinates, AUTH_TOKEN } from './constants'
 
 const API_URL = `https://api.openweathermap.org/data/2.5/weather?lat=${coordinates.lat}&lon=${coordinates.long}&units=imperial&appid=${AUTH_TOKEN}`
 
-// const weatherObject = {
-//   coord: {
-//     lon: -77.0369,
-//     lat: 38.9072,
-//   },
-//   weather: [
-//     {
-//       id: 800,
-//       main: 'Clear',
-//       description: 'clear sky',
-//       icon: '01n',
-//     },
-//   ],
-//   base: 'stations',
-//   main: {
-//     temp: 29.26,
-//     feels_like: 29.26,
-//     temp_min: 26.29,
-//     temp_max: 32.32,
-//     pressure: 1026,
-//     humidity: 67,
-//   },
-//   visibility: 10000,
-//   wind: {
-//     speed: 0,
-//     deg: 0,
-//   },
-//   clouds: {
-//     all: 0,
-//   },
-//   dt: 1675303823,
-//   sys: {
-//     type: 2,
-//     id: 2008521,
-//     country: 'US',
-//     sunrise: 1675253679,
-//     sunset: 1675290519,
-//   },
-//   timezone: -18000,
-//   id: 4140963,
-//   name: 'Washington D.C.',
-//   cod: 200,
-// }
-
 // here is the code of the checking the response status
 function checkResponse(res) {
   if (res.ok) {
@@ -54,7 +10,7 @@ function checkResponse(res) {
   return Promise.reject(`Error: ${res.status}`)
 }
 
-export function request() {
+export function requestWeatherApiData() {
   return fetch(API_URL).then(checkResponse)
 }
 
