ce5a81dad26c5f8e91b1df37d1fe6d9525402b86
Ran Pol <rnd809@gmail.com>
2023-02-12 22:42:18 -0500

    Create the Profile component that renders the sidebar and the clothes list.

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,7 +1,15 @@
 import React from 'react'
+import SideBar from './SideBar/SideBar'
+import ClothesSection from './ClothesSection/ClothesSection'
+import './Profile.css'
 
 const Profile = () => {
-  return <div>Profile</div>
+  return (
+    <div className="profile page__profile">
+      <SideBar />
+      <ClothesSection />
+    </div>
+  )
 }
 
 export default Profile
