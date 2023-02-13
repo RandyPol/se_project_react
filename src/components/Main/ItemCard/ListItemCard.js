39897c207a5834d46e34262f724dfedab606c4e0
Ran Pol <rnd809@gmail.com>
2023-02-12 22:27:45 -0500

    Delete the extra div and allow the Li to be the container.

--- src/components/Main/ItemCard/ListItemCard.js
+++ src/components/Main/ItemCard/ListItemCard.js
@@ -1,11 +1,11 @@
+import cx from 'classnames'
 import React from 'react'
 import './ListItemCard.css'
 
-const ItemCard = ({ item, weather, handleItemModalToggleOpen }) => {
+const ItemCard = ({ className, item, weather, handleItemModalToggleOpen }) => {
   const cardInfo = { ...item, weather }
   return (
-    <li>
-      <div className="card">
+    <li className={cx(className, "card")}>
         <h3 className="card__title">{item.name}</h3>
         <img
           onClick={() => handleItemModalToggleOpen(cardInfo)}
@@ -13,7 +13,6 @@ const ItemCard = ({ item, weather, handleItemModalToggleOpen }) => {
           src={item.link}
           alt={item.name}
         />
-      </div>
     </li>
   )
 }
