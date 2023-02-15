e96c00c29eb3b2988d5dc61654e01a66f02c3c74
Ran Pol <rnd809@gmail.com>
2023-02-15 17:43:58 -0500

    Delete the object spread from ListItemCard.

--- src/components/Main/ItemCard/ListItemCard.js
+++ src/components/Main/ItemCard/ListItemCard.js
@@ -3,12 +3,11 @@ import React from 'react'
 import './ListItemCard.css'
 
 const ItemCard = ({ className, item, handleItemModalToggleOpen }) => {
-  const cardInfo = { ...item }
   return (
     <li className={cx(className, 'card')}>
       <h3 className="card__title">{item.name}</h3>
       <img
-        onClick={() => handleItemModalToggleOpen(cardInfo)}
+        onClick={() => handleItemModalToggleOpen(item)}
         className="card__image"
         src={item.imageUrl}
         alt={item.name}
