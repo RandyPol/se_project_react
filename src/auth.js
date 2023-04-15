c5ebeaeffbfa49c87f8f6c2bb3fdfcbba6b7bbfa
Ran Pol <rnd809@gmail.com>
2023-04-15 19:10:13 -0400

    Add authorization logic.

--- src/auth.js
+++ src/auth.js
@@ -1,8 +1,7 @@
 export const BASE_URL = 'http://localhost:3000'
 
-
 /**
- * 
+ *
  * @param {*} name this is the name of the user
  * @param {*} avatar this is the avatar of the user
  * @param {*} email this is the email of the user
@@ -35,4 +34,37 @@ export const register = async (name, avatar, email, password) => {
   }
 }
 
+/**
+ *
+ * @param {*} email this is the email of the user
+ * @param {*} password this is the password of the user
+ * @returns {Promise} this is the promise of the request to the server. It returns the user data if the request is successful, otherwise it returns null.
+ *
+ * */
 
+export const authorize = async (email, password) => {
+  try {
+    const response = await fetch(`${BASE_URL}/signin`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        email,
+        password,
+      }),
+    })
+    if (!response.ok) {
+      throw new Error('Some error happend in the server')
+    }
+    const data = await response.json()
+    if (data.token) {
+      localStorage.setItem('token', data.token)
+      return data
+    }
+    return null
+  } catch (error) {
+    console.error(error)
+    return null
+  }
+}
