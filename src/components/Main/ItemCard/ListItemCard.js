24bdf32b3f0381887247fee77760dbf19517fd25
Ran Pol <rnd809@gmail.com>
2023-04-17 17:16:55 -0400

    Create the UI of the heart icon.

--- src/components/Main/ItemCard/ListItemCard.js
+++ src/components/Main/ItemCard/ListItemCard.js
@@ -1,11 +1,17 @@
 import cx from 'classnames'
 import React from 'react'
+import { ReactComponent as HeartIcon } from '../../../images/heart.svg'
+
 import './ListItemCard.css'
 
 const ItemCard = ({ className, item, handleItemModalToggleOpen }) => {
   return (
     <li className={cx(className, 'card')}>
-      <h3 className="card__title">{item.name}</h3>
+      <div className="card__title-container">
+        <h3 className="card__title">{item.name}</h3>
+        <HeartIcon className="card__heart" />
+      </div>
+
       <img
         onClick={() => handleItemModalToggleOpen(item)}
         className="card__image"
