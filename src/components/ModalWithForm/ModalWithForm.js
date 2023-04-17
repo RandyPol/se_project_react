bc03dfae60ebd470009539d22848df8e1d74691f
Ran Pol <rnd809@gmail.com>
2023-04-17 03:04:07 -0400

    Create the UI for the or buttons.

--- src/components/ModalWithForm/ModalWithForm.js
+++ src/components/ModalWithForm/ModalWithForm.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import Popup from '../Popup/Popup'
 import './ModalWithForm.css'
+import CurrentUserContext from '../../contexts/CurrentUserContext'
 
 const ModalWithForm = ({
   title,
@@ -12,26 +13,48 @@ const ModalWithForm = ({
   isSubmitDisabled,
   isOpen,
 }) => {
+  const { currentUser } = React.useContext(CurrentUserContext)
+
   return (
     <Popup isOpen={isOpen} onClose={handleFormToggleOpen} containerName={name}>
       <form className="form" name={name} onSubmit={handleSubmit} noValidate>
         <h2 className="form__title">{title}</h2>
 
         <div className="form__body">{children}</div>
-
-        <fieldset className="form__fieldset">
-          <button
-            type="submit"
-            disabled={isSubmitDisabled}
-            className={
-              isSubmitDisabled
-                ? 'form__submit-button_disabled'
-                : 'form__submit-button_base'
-            }
-          >
-            {buttonText}
-          </button>
-        </fieldset>
+        {currentUser._id ? (
+          <fieldset className="form__fieldset">
+            <button
+              type="submit"
+              disabled={isSubmitDisabled}
+              className={
+                isSubmitDisabled
+                  ? 'form__submit-button_disabled'
+                  : 'form__submit-button_base'
+              }
+            >
+              {buttonText}
+            </button>
+          </fieldset>
+        ) : (
+          <fieldset className="form__fieldset">
+            <div>
+              <button
+                type="submit"
+                disabled={isSubmitDisabled}
+                className={
+                  isSubmitDisabled
+                    ? 'form__submit-button_disabled'
+                    : 'form__submit-button_base'
+                }
+              >
+                {buttonText}
+              </button>
+              <button type="button" className="form__submit-orButton">
+                {buttonText === 'Next' ? 'or Log in' : 'or Register'}
+              </button>
+            </div>
+          </fieldset>
+        )}
       </form>
     </Popup>
   )
