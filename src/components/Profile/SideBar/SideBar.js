61b9fb0b58a6296ceab9b776417be7c92a6bfb96
Ran Pol <rnd809@gmail.com>
2023-04-16 23:29:45 -0400

    Update the side profile data with current user info,

--- src/components/Profile/SideBar/SideBar.js
+++ src/components/Profile/SideBar/SideBar.js
@@ -1,16 +1,29 @@
 import React from 'react'
-import avatar from '../../../images/avatar.avif'
 import './SideBar.css'
+import CurrentUserContext from '../../../contexts/CurrentUserContext'
 
 const SideBar = () => {
+  // Get the current user from the context
+  const currentUser = React.useContext(CurrentUserContext)
+  // Get user initials
+  const getUserInitial = () => {
+    const name = currentUser.name
+    if (name) return name.charAt(0).toUpperCase()
+  }
   return (
     <div className="sidebar profile__sidebar">
-      <img
-        src={avatar}
-        className="sidebar__profile-avatar"
-        alt="Profile avatar"
-      />
-      <p className="sidebar__username">Michael Jordan</p>
+      {currentUser.avatar ? (
+        <img
+          src={currentUser.avatar}
+          className="sidebar__profile-avatar"
+          alt="Profile avatar"
+        />
+      ) : (
+        <div className="sidebar__profile-avatar sidebar__profile-initials">
+          {getUserInitial()}
+        </div>
+      )}
+      <p className="sidebar__username">{currentUser.name}</p>
     </div>
   )
 }
