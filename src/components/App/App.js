777ca07f4bd0cd04d1c4c9b71d06408f31245c99
Ran Pol <rnd809@gmail.com>
2023-04-24 22:09:11 -0400

    Import the moduler code from the hooks.

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -21,8 +21,7 @@ import useAuth from '../../hooks/useAuth'
 import useWeather from '../../hooks/useWeather'
 import useClothingItems from '../../hooks/useClothingItems'
 import useModalStates from '../../hooks/useModalStates'
-
-import api from '../../utils/api'
+import useActions from '../../hooks/useActions'
 
 function App() {
   const { loggedIn, checkingAuth, currentUser, setCurrentUser, setLoggedIn } =
@@ -36,174 +35,42 @@ function App() {
     isRegisterFormOpen,
     isLoginFormOpen,
     isProfileEditFormOpen,
-    setIsModalFormOpen,
-    setIsItemModalOpen,
-    setIsDeleteModalOpen,
-    setIsRegisterFormOpen,
-    setIsLoginFormOpen,
-    setIsProfileEditFormOpen,
+    handleFormToggleOpen,
+    handleRegisterModalToggleOpen,
+    handleLoginModalToggleOpen,
+    handleDeleteModalToggleOpen,
+    handleProfileEditModalToggleOpen,
+    ItemModalToggleOpen,
   } = useModalStates()
+  const {
+    isTempFahrenheit,
+    cardItem,
+    setCardItem,
+    handleLogin,
+    handleTempUnitToggle,
+    handleAddItemSubmit,
+    handleCardDelete,
+    handleRegister,
+    isLoading,
+    handleProfileEdit,
+    handleCardLike,
+    handleLogout,
+  } = useActions(
+    clothingItems,
+    setCurrentUser,
+    setLoggedIn,
+    setClothingItems,
+    handleFormToggleOpen,
+    handleDeleteModalToggleOpen,
+    handleRegisterModalToggleOpen,
+    handleLoginModalToggleOpen,
+    handleProfileEditModalToggleOpen
+  )
 
-
-  const [isTempFahrenheit, setIsTempFahrenheit] = React.useState(true)
-
-  // Card item info for the Card that was clicked to open the ItemModal
-  const [cardItem, setCardItem] = React.useState({})
-
-  // Toggle the temperature unit between Fahrenheit and Celsius
-  const handleTempUnitToggle = () => {
-    setIsTempFahrenheit((prev) => !prev)
-  }
-
-  // This is the function to toggle open and close the form modal
-  const handleFormToggleOpen = () => {
-    setIsModalFormOpen((prevs) => !prevs)
-  }
-
-  // Handle the toggle for the RegisterModal
-  const handleRegisterModalToggleOpen = () => {
-    setIsRegisterFormOpen((prevs) => !prevs)
-  }
-
-  // Handle the toggle for the LoginModal
-  const handleLoginModalToggleOpen = (status = true) => {
-    if (status) setIsLoginFormOpen((prevs) => !prevs)
-  }
-
-  // Handle the toggle for the ProfileEditModal
-  const handleProfileEditModalToggleOpen = () => {
-    setIsProfileEditFormOpen((prevs) => !prevs)
-  }
-
-  // Handle the toggle for the ItemModal
+  // Handle the toggle for the ItemModal With Card Info
   const handleItemModalToggleOpen = (cardInfo) => {
     setCardItem(cardInfo)
-    setIsItemModalOpen((prevs) => !prevs)
-  }
-  // Handle the toggle for the DeleteModal
-  const handleDeleteModalToggleOpen = () => {
-    setIsDeleteModalOpen((prevs) => !prevs)
-  }
-  // Add a new clothing item to the server
-  const handleAddItemSubmit = (newItem) => {
-    setIsLoading((prev) => !prev)
-    return api
-      .postClothingItem(newItem)
-      .then((addedData) => {
-        setClothingItems([addedData, ...clothingItems])
-        setIsModalFormOpen((prevs) => !prevs)
-        return Promise.resolve()
-      })
-      .catch((err) => {
-        console.log(err)
-      })
-      .finally(() => {
-        setIsLoading((prev) => !prev)
-      })
-  }
-
-  // Delete a clothing item from the server
-  const handleCardDelete = (id) => {
-    setIsLoading((prev) => !prev)
-    api
-      .deleteClothesItem(id)
-      .then(() => {
-        const newClothingItems = clothingItems.filter((item) => {
-          return item._id !== id
-        })
-        setClothingItems(newClothingItems)
-        handleDeleteModalToggleOpen()
-      })
-      .catch((err) => console.log(err))
-      .finally(() => {
-        setIsLoading((prev) => !prev)
-      })
-  }
-
-  // Handle Register
-  const handleRegister = ({ name, avatar, email, password }) => {
-    setIsLoading((prev) => !prev)
-    auth
-      .register(name, avatar, email, password)
-      .then((data) => {
-        if (data) {
-          handleRegisterModalToggleOpen()
-          handleLogin({ email, password }, false)
-        }
-      })
-      .catch((err) => console.log(err))
-      .finally(() => {
-        setIsLoading((prev) => !prev)
-      })
-  }
-
-  // Handle Login and set the JWT token
-  const handleLogin = ({ email, password }, close = true) => {
-    setIsLoading((prev) => !prev)
-    auth
-      .authorize(email, password)
-      .then((data) => {
-        if (data.token) {
-          localStorage.setItem('jwt', JSON.stringify(data.token))
-          return auth.checkToken(data.token)
-        } else {
-          throw new Error('Token not received')
-        }
-      })
-      .then((userData) => {
-        setCurrentUser(userData)
-        setLoggedIn(true)
-        handleLoginModalToggleOpen(close)
-      })
-      .catch((err) => console.log(err))
-      .finally(() => {
-        setIsLoading((prev) => !prev)
-      })
-  }
-
-  // Handle Profile Edit and update the user info
-  const handleProfileEdit = ({ name, avatar }) => {
-    setIsLoading((prev) => !prev)
-    api
-      .updateProfile(name, avatar)
-      .then((data) => {
-        setCurrentUser(data)
-        handleProfileEditModalToggleOpen()
-      })
-      .catch((err) => console.log(err))
-      .finally(() => {
-        setIsLoading((prev) => !prev)
-      })
-  }
-
-  // Handle card like
-  const handleCardLike = (id, isLiked) => {
-    isLiked
-      ? api
-          .removeCardLike(id)
-          .then((updatedCard) => {
-            const newCards = clothingItems.map((c) =>
-              c._id === id ? updatedCard : c
-            )
-            setClothingItems(newCards)
-          })
-          .catch((err) => console.log(err))
-      : api
-          .addCardLike(id)
-          .then((updatedCard) => {
-            const newCards = clothingItems.map((c) =>
-              c._id === id ? updatedCard : c
-            )
-            setClothingItems(newCards)
-          })
-          .catch((err) => console.log(err))
-  }
-
-  // Handle Logout and remove the JWT token
-  const handleLogout = () => {
-    localStorage.removeItem('jwt')
-    setLoggedIn(false)
-    setCurrentUser({})
+    ItemModalToggleOpen()
   }
 
   return (
